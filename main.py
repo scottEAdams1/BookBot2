ac8601a32f99080cfa229c02da581208cca76ba4
scottEAdams1 <scott.e.adams01@gmail.com>
2025-06-03 12:44:17 +0100

    Can count number of each character in document

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
 #Reads out contents of book
 def main():
@@ -6,6 +6,10 @@ def main():
     contents = get_book_text(filepath)
     num_words = get_num_words(contents)
     print(f'{num_words} words found in the document')
+    num_chars = get_num_chars(contents)
+    print('Count of each char:')
+    for char, count in num_chars.items():
+        print(f"\t'{char}': {count}")
 
 
 #Takes filepath for a book, returns contents of file as a string
--- stats.py
+++ stats.py
@@ -3,3 +3,15 @@ def get_num_words(text):
     words = text.split()
     num_words = len(words)
     return num_words
+
+
+#Takes text as string, returns number of instances for each character as a dictionary
+def get_num_chars(text):
+    dictionary = {}
+    for char in text:
+        char = char.lower()
+        if char in dictionary:
+            dictionary[char] += 1
+        else:
+            dictionary[char] = 1
+    return dictionary
